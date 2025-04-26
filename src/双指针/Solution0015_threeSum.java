5c402ed818bbad6f6c197866b32edb1ad1e55149
lys1313013 <lys1313013@gmail.com>
2025-04-26 23:03:14 +0800

    feat: 15. 三数之和 双指针

---
+++ src/双指针/Solution0015_threeSum.java
@@ -0,0 +1,46 @@
+package 双指针;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 15. 三数之和
+ * https://leetcode.cn/problems/3sum/description/?envType=study-plan-v2&envId=top-100-liked
+ */
+class Solution0015_threeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            // 过滤重复数据
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int target = -nums[i];
+            int l = i + 1;
+            int r = n - 1;
+            while (l < r) {
+                if (nums[l] + nums[r] == target) {
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    l++;
+                    r--;
+
+                    // 去重
+                    while (l < r && nums[l] == nums[l - 1]) {
+                        l++;
+                    }
+                    while (l < r && nums[r] == nums[r + 1]) {
+                        r--;
+                    }
+                } else if (nums[l] + nums[r] > target) {
+                    r--;
+                } else {
+                    l++;
+                }
+            }
+        }
+        return res;
+    }
+}
