bc72da6b6c5e134962a60cbfd9c2f16724fb482a
lys1313013 <lys1313013@gmail.com>
2025-05-07 21:18:46 +0800

    19. 删除链表的倒数第 N 个结点

---
+++ src/链表/Solution0019_removeNthNodeFromEndOfList.java
@@ -0,0 +1,27 @@
+package 链表;
+
+/**
+ * 19. 删除链表的倒数第 N 个结点
+ * https://leetcode.cn/problems/remove-nth-node-from-end-of-list
+ */
+class Solution0019_removeNthNodeFromEndOfList {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode tempHead = head;
+        ListNode slow = head;
+        ListNode fast = head;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        if (fast == null) {
+            return slow.next;
+        }
+
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+
+        return tempHead;
+    }
+}
