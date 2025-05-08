e43b20fd1cd5c59fa0808cc06a96f1cfd34d10c7
lys1313013 <lys1313013@gmail.com>
2025-05-08 21:55:15 +0800

    94. 二叉树的中序遍历

---
+++ src/二叉树/Solution0094_binaryTreeInorderTraversal.java
@@ -0,0 +1,25 @@
+package 二叉树;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 94. 二叉树的中序遍历
+ * https://leetcode.cn/problems/binary-tree-inorder-traversal/
+ */
+class Solution0094_binaryTreeInorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorder(root, res);
+        return res;
+    }
+
+    public void inorder(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, res);
+        res.add(root.val);
+        inorder(root.right, res);
+    }
+}
