fc42a482ea2dd55a758ce9112f55a2371e4ead9c
lys1313013 <lys1313013@gmail.com>
2025-05-22 22:44:43 +0800

    3355. 零数组变换 I #前缀和

---
+++ src/前缀和/Solution.java
@@ -0,0 +1,36 @@
+package 前缀和;
+
+/**
+ * 3355. 零数组变换 I
+ * 前缀和
+ */
+class Solution {
+    public boolean isZeroArray(int[] nums, int[][] queries) {
+        int[] deltaArray = new int[nums.length + 1];
+        for (int[] query : queries) {
+            int left = query[0];
+            int right = query[1];
+            deltaArray[left] += 1;
+            deltaArray[right + 1] -= 1;
+        }       
+        int[] operationCounts = new int[deltaArray.length];
+        int currentOperations = 0;
+        for (int i = 0; i < deltaArray.length; i++) {
+            currentOperations += deltaArray[i];
+            operationCounts[i] = currentOperations;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (operationCounts[i] < nums[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {4, 3, 2, 1};
+        int[][] queries = {{1,3}, {0,2}};
+        boolean result = solution.isZeroArray(nums, queries);
+    }
+}
