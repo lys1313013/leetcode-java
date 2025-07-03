1a738392369f0acbf0d72f3012a49fa041557a55
lys1313013 <lys1313013@gmail.com>
2025-07-03 21:57:09 +0800

    347. 前 K 个高频元素

---
+++ src/堆/Solution0347.java
@@ -0,0 +1,44 @@
+package 堆;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/**
+ * 347. 前 K 个高频元素
+ */
+class Solution0347 {
+    public int[] topKFrequent(int[] nums, int k) {
+        // 使用字典，统计每个元素出现的次数，元素为健，元素出现的次数为值
+        Map<Integer, Integer> map = new HashMap();
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+        // 遍历map,用最小堆保存频率最大的k个元素
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer a, Integer b) {
+                return map.get(a) - map.get(b);
+            }
+        });
+        for (Integer key : map.keySet()) {
+            if (pq.size() < k) {
+                pq.add(key);
+            } else if (map.get(key) > map.get(pq.peek())) {
+                pq.remove();
+                pq.add(key);
+            }
+        }
+        int[] res = new int[pq.size()];
+        int i = 0;
+        while (!pq.isEmpty()) {
+            res[i++] = pq.remove();
+        }
+        return res;
+    }
+}
